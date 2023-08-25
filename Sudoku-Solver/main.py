2bae4c1a822fae1313ea90afaeb8a6f006109081
Siddesh R Ohri <siddeshohrri@gmail.com>
2023-08-25 20:56:04 +0100

    Modifed the code to include an HTML display

--- Sudoku-Solver/main.py
+++ Sudoku-Solver/main.py
@@ -1,37 +1,38 @@
+from flask import Flask, render_template
 from Backtracking import sudoku_solve
 import test_cases
 
-def print_grid(grid):
+app = Flask(__name__)
 
+def format_grid(grid):
     """
-    Print the given Sudoku grid in a readable format.
+    Format the given Sudoku grid as a list of lists of strings for rendering.
 
     Args:
-        grid (list[list[int]]): The Sudoku grid to be printed.
-    """
+        grid (list[list[int]]): The Sudoku grid to be formatted.
 
+    Returns:
+        list[list[str]]: Formatted grid as strings.
+    """
+    formatted_grid = []
     for row in grid:
-        for num in row:
-            print(num, end=" ")
-        print()
+        formatted_row = [str(num) if num != 0 else '' for num in row]
+        formatted_grid.append(formatted_row)
+    return formatted_grid
 
-def main(grid):
+@app.route('/')
+def index():
+    results = []
 
-    """
-    Main function to solve and print a Sudoku puzzle.
-    """
+    for test_case in [test_cases.test_case_1, test_cases.test_case_2, test_cases.test_case_3, test_cases.test_case_4]:
+        grid_copy = [row[:] for row in test_case]  # Create a copy of the grid
+        if sudoku_solve(grid_copy):
+            formatted_grid = format_grid(grid_copy)
+            results.append(formatted_grid)
+        else:
+            results.append("No solution exists.")
 
-    if sudoku_solve(grid):
-        print_grid(grid)
-    else:
-        print("No solution exists.")
+    return render_template('index.html', results=results)
 
 if __name__ == "__main__":
-    main(test_cases.test_case_1)
-    print()
-    main(test_cases.test_case_2)
-    print()
-    main(test_cases.test_case_3)
-    print()
-    main(test_cases.test_case_4)
-    print()
+    app.run(debug=True)
