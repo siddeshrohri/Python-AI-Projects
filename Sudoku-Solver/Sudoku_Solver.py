c7cc2fec72c4759510611539c1a301915080868e
Siddesh R Ohri <siddeshohrri@gmail.com>
2023-08-25 14:41:04 +0100

    Condition for Valid Moves

--- Sudoku-Solver/Sudoku_Solver.py
+++ Sudoku-Solver/Sudoku_Solver.py
@@ -1,3 +1,24 @@
+def is_valid_move(grid, row, col, num):
+    for x in range(9):
+        if grid[row][x] == num:
+            return False
+    for x in range(9):
+        if grid[x][col] == num:
+            return False
+        
+    # LOGIC 1 for BLOCK NUMBER CHECK
+    # corner_row = row - row % 3
+    # corner_col = col - col % 3
+    # for x in range(3):
+    #     for y in range(3):
+    #         if grid[corner_row + x][corner_col + y] == num:
+    #             return False
+
+    # LOGIC 2 for BLOCK NUMBER CHECK
+    for x in range(9):
+        if grid[3*(row//3)+x//3][3*(col//3)+x%3] == num:
+            return False
+
 grid = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
