9be264506e466e38ea6e8c71298e5a6e9be0baaa
Siddesh R Ohri <siddeshohrri@gmail.com>
2023-08-31 17:12:41 +0100

    Working JSON requests and Data extraction included imports for further tests

--- Crypto-Prediction/main.py
+++ Crypto-Prediction/main.py
@@ -1,7 +1,12 @@
 from tensorflow import keras
 import requests
+# from keras.models import Sequential
+# from keras.layers import Activation, Dense, Dropout, LSTM
+# import matplotlib.pyplot as plt
 import pandas as pd
+# import numpy as np
 import json
+# from sklearn.metrics import mean_absolute_error
 
 # Requests the data from the API
 res = requests.get('https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=500')
