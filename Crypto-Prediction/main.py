7cde9c127c0218db930029ce23fd31029e0a5123
Siddesh R Ohri <siddeshohrri@gmail.com>
2023-08-31 14:31:31 +0100

    All imports added

--- Crypto-Prediction/main.py
+++ Crypto-Prediction/main.py
@@ -1,25 +1,9 @@
-import numpy as np
+from tensorflow import keras
+import requests
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, LSTM
 import matplotlib.pyplot as plt
 import pandas as pd
-import pandas_datareader as web
-import datetime as dt
-
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow import keras
-from keras.layers import Dense, Dropout, LSTM
-from keras.models import Sequential
-
-'''Inputting the Data to be used for the model'''
-
-crypto_currency = 'BTC'
-against_currency = 'USD'
-
-start = dt.datetime(2016,1,1)
-end = dt.datetime.now()
-
-data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
-
-
-'''Preparing the data for the model to implement on'''
-
-print(data.head())
+import numpy as np
+import json
+from sklearn.metrics import mean_absolute_error
