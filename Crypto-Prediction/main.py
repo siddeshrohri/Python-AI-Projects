094c65ef94588d90cf592d2530632924ec6ee0ff
Siddesh R Ohri <siddeshohrri@gmail.com>
2023-09-07 15:05:33 +0100

    Included Alternative Versions for splitting the dataset

--- Crypto-Prediction/main.py
+++ Crypto-Prediction/main.py
@@ -4,7 +4,7 @@ import matplotlib as plt
 import numpy as np
 import json
 
-from sklearn.metrics import train_test_split
+from sklearn.model_selection import train_test_split
 
 # Using the API to extract the data
 # Requests the data from the API with the specified parameters "BTC and CAD, limit 500"
@@ -36,9 +36,17 @@ print(hist.head(5))
 # Storing a DataFrame for Training
 # Storing the DataFrame for Testing
 # Returning the values
+# Included an Alternative implementation as well
 def train_test_split(df, test_size=0.2):
     row = len(df) - int(test_size * len(df))
+    # Alternative calculation
+    # row = int(len(df) * (1 - test_size))
     train_data = df.iloc[:row]
     test_data = df.iloc[row:]
     return train_data, test_data
 train_test_split(hist, test_size=0.2)
+
+# Alternative Method for splitting the data into two sets 
+# def train_test_split(df, test_size=0.2):
+#     train_data, test_data = train_test_split(df, test_size=test_size, shuffle = False)
+#     return train_data, test_data
